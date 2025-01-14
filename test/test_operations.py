5a55bbc43f2ff5c5d5a75d30a90ebf0d1bfa1869
carvas91 <carvas91@hotmail.com>
2025-01-14 17:02:45 -0500

    Unit test case updated

---
+++ test/test_operations.py
@@ -0,0 +1,10 @@
+from src.math_operations import add, sub
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(0, 0) == 0
+
+def test_sub():
+    assert sub(1, 2) == -1
+    assert sub(0, 0) == 0
+    assert sub(2, 1) == 1
